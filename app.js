19504060ce8725834bddfdaa64c22ab7eae98022
notVLNT <211336718+notVLNT@users.noreply.github.com>
2026-04-03 17:29:02 +0200

    c

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log("Il file app.js è stato caricato correttamente!");
 const SUPABASE_URL = 'https://ejnbetqywcyukrtwetjt.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_P-sn36PRhmvlUVlQrpXBCw_ThF-dgWZ';
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
